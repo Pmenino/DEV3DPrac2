408cf91fee3147c573909207194c048c120f2682
Pmenino <pmenino@edu.tecnocampus.cat>
2021-11-01 00:01:24 +0100

    Portal medio medio

--- DEV3DPrac2/Assets/PortalCamera.cs
+++ DEV3DPrac2/Assets/PortalCamera.cs
@@ -5,18 +5,26 @@ using UnityEngine;
 public class PortalCamera : MonoBehaviour
 {
     [SerializeField] private PortalCamera otherPortal;
-    [SerializeField] Camera playerCamera;
+    [SerializeField] private Transform playerCameraTransform;
     [SerializeField] private Transform virtualPortal;
     [SerializeField] private Transform portalCamera;
 
-    [SerializeField]
+    [SerializeField] private float nearClipOffset;
+
     private void Update()
     {
-        Vector3 l_position = virtualPortal.InverseTransformPoint(playerCamera.transform.position);
+        Vector3 l_position = virtualPortal.InverseTransformPoint(playerCameraTransform.position);
         otherPortal.portalCamera.transform.position = otherPortal.transform.TransformPoint(l_position);
 
-        Vector3 l_direction = virtualPortal.InverseTransformPoint(playerCamera.transform.forward);
-        otherPortal.portalCamera.transform.forward = otherPortal.transform.TransformDirection(l_direction);
+        float playerDist = (playerCameraTransform.position - transform.position).magnitude;
 
+        otherPortal.portalCamera.GetComponent<Camera>().nearClipPlane = playerDist + nearClipOffset;
+
+        otherPortal.portalCamera.GetComponent<Camera>().fieldOfView = 120 / playerDist;
+
+        Vector3 local_direction = virtualPortal.InverseTransformDirection(playerCameraTransform.forward);
+        otherPortal.portalCamera.forward = otherPortal.transform.TransformDirection(local_direction);
+        otherPortal.portalCamera.GetComponent<Camera>().nearClipPlane =
+            (transform.position - playerCameraTransform.position).magnitude + nearClipOffset;
     }
 }
